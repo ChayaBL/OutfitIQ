c17d128a6972e3a2d9b433bdea14c25b8fdf9323
Chaya B.L <chayagowda.0609@gmail.com>
2026-07-03 19:36:00 +0530

    Handle signup form and redirect to sign in

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,9 +10,24 @@ def home():
 def signin():
     return render_template("signin.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        print(name)
+        print(email)
+        print(password)
+
+        return redirect(url_for("signin"))
+
     return render_template("signup.html")
+
+   
 
 if __name__ == "__main__":
     app.run(debug=True)
